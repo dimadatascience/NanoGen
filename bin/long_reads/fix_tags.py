7ce2cb95effec75ccbb9434a2d737cf9453b4df5
Andrea Cossa <IEO5505@Cossas-MBP.ieo.it>
2024-08-20 09:52:13 +0200

    correct bug fix_tags

--- bin/long_reads/fix_tags.py
+++ bin/long_reads/fix_tags.py
@@ -12,16 +12,16 @@ def main():
     path_input = sys.argv[1]
     path_output = 'CB_UB.bam'
 
-    input_bam = pysam.AlignmentFile(path_input, 'rb')
+    input_bam = pysam.AlignmentFile(path_input, 'rb', require_index=False)
     output_bam = pysam.AlignmentFile(path_output, 'wb', template=input_bam)
 
     for read in input_bam:
-        if read is not None:
+        if read.seq is not None:
             read_name = read.qname
             CB, UB = read_name[:29].split('_')
             read.qname = read_name[30:]
-            read = read.set_tag('CB', CB)
-            read = read.set_tag('UB', UB)
+            read.set_tag('CB', CB)
+            read.set_tag('UB', UB)
             output_bam.write(read)
 
     input_bam.close()
